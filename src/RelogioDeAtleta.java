653f4762d6532fa8a78a07016aca3ac49f0b0948
Gustavo Gonçalo <gustavogr1806@gmail.com>
2022-04-04 09:31:13 -0300

    lmpl de entrada de dados

--- src/RelogioDeAtleta.java
+++ src/RelogioDeAtleta.java
@@ -1,9 +1,18 @@
+import java.util.*;
 
 public class RelogioDeAtleta {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Informe a Idade: ");
+		int idade = ler.nextInt();
+		
+		System.out.println("Informe o Salário");
+		double salario = ler.nextDouble();
+		
+		
 	}
 
 }
