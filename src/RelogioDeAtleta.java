7a897570d441d1976ca8fd7ca7c090391f435b4d
Gustavo Gonçalo <gustavogr1806@gmail.com>
2022-04-04 09:55:36 -0300

    Finalização do Projeto

--- src/RelogioDeAtleta.java
+++ src/RelogioDeAtleta.java
@@ -1,3 +1,7 @@
+/*
+  Nome: Gustavo Gonçalo Rodrigues
+*/
+
 import java.util.*;
 
 public class RelogioDeAtleta {
@@ -6,13 +10,35 @@ public class RelogioDeAtleta {
 		
 		Scanner ler = new Scanner(System.in);
 		
+		int frequenciaCardiacaAtual;
+		System.out.println("Informe sua Frequência Cardíaca Atual: ");
+		frequenciaCardiacaAtual = ler.nextInt();
+		
+		int frequenciaCardiacaRepouso;
+		System.out.println("Informe sua Frequência Cardíaca em Repouso: ");
+		frequenciaCardiacaRepouso = ler.nextInt();
+		
+		int oxigenacao;
+		System.out.println("Informe a Oxigenação Atual: ");
+		oxigenacao = ler.nextInt();
+		
+		if (frequenciaCardiacaAtual > frequenciaCardiacaRepouso * 3 || oxigenacao < 95) {
+			System.out.println("diminuir");
+		}
+		else if (frequenciaCardiacaAtual < frequenciaCardiacaRepouso * 2 || oxigenacao > 97) {
+			System.out.println("aumentar");
+		}
+		else {
+			System.out.println("manter");
+		}
+		
+		/*
 		System.out.println("Informe a Idade: ");
 		int idade = ler.nextInt();
 		
 		System.out.println("Informe o Salário");
 		double salario = ler.nextDouble();
-		
-		
+		*/
 	}
 
 }
